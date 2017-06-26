86a9589e921042f6e1755ebc55e1bf544f65e97e
sebastian.waschnick <sebastian@waschnick.de>
2017-06-26 20:24:44 +0200

    WIP

--- src/main/java/de/axelspringer/ideas/team/mood/TeamMoodProperties.java
+++ src/main/java/de/axelspringer/ideas/team/mood/TeamMoodProperties.java
@@ -18,36 +18,44 @@ public class TeamMoodProperties {
 
     public String getUsername() {
         if (username == null) {
-            username = System.getProperty("de.axelspringer.ideas.team.mood.mail.user");
+            username = getProperty("de.axelspringer.ideas.team.mood.mail.user");
         }
         return username;
     }
 
     public String getPassword() {
         if (password == null) {
-            password = System.getProperty("de.axelspringer.ideas.team.mood.mail.password");
+            password = getProperty("de.axelspringer.ideas.team.mood.mail.password");
         }
         return password;
     }
 
     public List<String> getTeamApiKeys() {
         if (teamApiKeys == null) {
-            teamApiKeys = Arrays.asList(System.getProperty("de.axelspringer.ideas.team.mood.api.keys").split(","));
+            teamApiKeys = Arrays.asList(getProperty("de.axelspringer.ideas.team.mood.api.keys").split(","));
         }
         return teamApiKeys;
     }
 
     public List<String> getEmailAddresses() {
         if (emails == null) {
-            emails = Arrays.asList(System.getProperty("de.axelspringer.ideas.team.mood.email.addresses").split(","));
+            emails = Arrays.asList(getProperty("de.axelspringer.ideas.team.mood.email.addresses").split(","));
         }
         return emails;
     }
 
     public String getBasicAuthPassword() {
         if (basicAuthPassword == null) {
-            basicAuthPassword = System.getProperty("de.axelspringer.ideas.team.mood.basicAuthPassword");
+            basicAuthPassword = getProperty("de.axelspringer.ideas.team.mood.basicAuthPassword");
         }
         return basicAuthPassword;
+    }
+
+    private String getProperty(String s) {
+        String result = System.getProperty(s);
+        if (result == null) {
+            result = System.getenv(s);
+        }
+        return result;
     }
 }
